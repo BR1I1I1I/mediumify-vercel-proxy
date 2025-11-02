1b745bd0a444896cc308ce33a23e72e7bfd469c2
BR1I1I1I <br1officialprod@gmail.com>
2025-11-03 02:14:50 +0300

    Update spotify.js

--- api/auth/spotify.js
+++ api/auth/spotify.js
@@ -1,9 +1,15 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const scopes = "user-read-private user-read-email";
   const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
   const clientId = process.env.SPOTIFY_CLIENT_ID;
+
+  if (!clientId || !redirectUri) {
+    return res.status(500).json({ error: "Missing environment variables" });
+  }
+
   const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(
     scopes
   )}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
   return res.redirect(authUrl);
 }
