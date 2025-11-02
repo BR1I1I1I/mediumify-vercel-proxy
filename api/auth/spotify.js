57e6cd6f632ab1e37fc33edc69e97c72877cfcb9
BR1I1I1I <br1officialprod@gmail.com>
2025-11-03 02:04:57 +0300

    Update spotify.js

--- api/auth/spotify.js
+++ api/auth/spotify.js
@@ -1,15 +1,9 @@
-import axios from "axios";
-
 export default function handler(req, res) {
-  if (req.method === "GET") {
-    const scopes = "user-read-private user-read-email";
-    const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
-    const clientId = process.env.SPOTIFY_CLIENT_ID;
-    const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(
-      scopes
-    )}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-    return res.redirect(authUrl);
-  }
-
-  return res.status(405).json({ error: "Method not allowed" });
+  const scopes = "user-read-private user-read-email";
+  const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(
+    scopes
+  )}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+  return res.redirect(authUrl);
 }
